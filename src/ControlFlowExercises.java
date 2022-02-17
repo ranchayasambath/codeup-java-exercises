4dbbddb3eba434fe611218e91dd3ad26396f8d3a
ranchayasambath <ranchayasambath@gmail.com>
2022-02-17 13:52:37 -0600

    completed 2.c

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,10 +5,16 @@ public class ControlFlowExercises {
 //            System.out.print(i + " ");
 //            i++;
 //        }
-        long count = 2;
-        do{
-            System.out.println(count);
-            count *= count;
-        }while(count < 1000000);
+
+//        long count = 2;
+//        do{
+//            System.out.println(count);
+//            count *= count;
+//        }while(count < 1000000);
+
+//        for(long i = 2; i < 1000000; i*=i){
+//            System.out.println(i);
+//        }
+
     }
 }
