648e2745f8f4c8fb00ce90e867905e3b145e710c
ranchayasambath <ranchayasambath@gmail.com>
2022-02-17 11:44:32 -0600

    completed step 1 a.

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,9 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+        int i = 5;
+        while (i <= 15){
+            System.out.println("the value of i is: " + i);
+            i++;
+        }
+    }
+}
