9e473b3a8d80b6448702d95a2df57e3639bbb0b6
ranchayasambath <ranchayasambath@gmail.com>
2022-02-17 13:41:36 -0600

    count 0 to 100

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,15 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15){
-            System.out.println("the value of i is: " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.println("the value of i is: " + i);
+//            i++;
+//        }
+        int count = 0;
+        do{
+            System.out.println("the value of i is: " + count);
+            count = count + 2;
+
+        }while(count < 100);
     }
 }
