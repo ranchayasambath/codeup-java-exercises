7a415f601aae9d22697a38568798df0af3b2d51a
ranchayasambath <ranchayasambath@gmail.com>
2022-02-17 10:16:36 -0600

    completed exercise

--- src/ConsoleExercise.java
+++ src/ConsoleExercise.java
@@ -7,46 +7,58 @@ public class ConsoleExercise {
 //        double pi = 3.14159;
 //        System.out.format("The value of pi is approximately %.2f.%n",pi);
 
-//        Scanner scan = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
 //        System.out.println("Enter an integer: ");
-//        int userInput = scan.nextInt();
+//        int userInput = scanner.nextInt();
 //        System.out.println("You Entered \"" + userInput + "\"");
 
         //2.
-//        Scanner one = new Scanner(System.in);
-//        System.out.print("Enter first word: ");
-//        String input1 = one.next();
-//
-//        Scanner two = new Scanner(System.in);
-//        System.out.print("Enter second word: ");
-//        String input2 = two.next();
-//
-//        Scanner three = new Scanner(System.in);
-//        System.out.print("Enter third word: ");
-//        String input3 = three.next();
-//        System.out.println("You Entered:\n"+ input1 + "\n" + input2 + "\n" + input3);
+//        System.out.print("Enter Three words: ");
+//        String input1 = scanner.next();
+//        String input2 = scanner.next();
+//        String input3 = scanner.next();
+////        System.out.println("You Entered:\n"+ input1 + "\n" + input2 + "\n" + input3);
+////        OR
+//        System.out.printf("%s%n%s%n%s%n",input1,input2,input3);
 
 //        3.
-//        Scanner sentence = new Scanner(System.in);
 //        System.out.print("Write a sentence: ");
 ////        String input = sentence.next();
-////        4.
+////       4.
 //        String input = sentence.nextLine();
 //        System.out.println(input);
 
 //Calculate parameter.
 //        1.
-        Scanner length = new Scanner(System.in);
-        System.out.print("Enter Length: ");
-        int input = Integer.parseInt(length.nextLine());
-
-        Scanner width = new Scanner(System.in);
-        System.out.print("Enter Width: ");
-        int input2 = Integer.parseInt(width.nextLine());
-
+//        System.out.print("Enter Length: ");
+//        int input = Integer.parseInt(scanner.next());
+//        System.out.print("Enter Width: ");
+//        int input2 = Integer.parseInt(scanner.next());
 //        2.
-        System.out.println("Your classroom parameter is " + (input+input2)*2 + " feet.") ;
+//        System.out.println("Your classroom parameter is " + (input+input2)*2 + " feet.");
+        //BONUS:
+        System.out.print("Enter Length: ");
+        double input = Double.parseDouble(scanner.next());
+        System.out.print("Enter Width: ");
+        double input2 = Double.parseDouble(scanner.next());
+        System.out.print("Enter Height: ");
+        double input3 = Double.parseDouble(scanner.next());
+//        PARAMETER:
+//        System.out.println("Your classroom parameter is " + (input+input2)*2 + " feet.");
+////        AREA:
+//        System.out.println("Your classroom area is " + (input*input2) + " square feet.");
+////        VOLUME:
+//        System.out.println("Your classroom volume is " + (input*input2*input3) + " cubic feet.");
+//        ALL CALCULATIONS:
+        System.out.println("Your classroom parameter is " + (input+input2)*2 + " feet." + "\nYour classroom area is " + (input*input2) + "square feet." + "\nYour classroom volume is "  + (input*input2*input3) + " cubic feet.");
+
+
+
+
+
+//
+
 
     }
-
 }
