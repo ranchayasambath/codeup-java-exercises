30d74831f21f9ae21f529fa48bf40f6ed4addfa7
ranchayasambath <ranchayasambath@gmail.com>
2022-02-18 12:28:40 -0600

    updated exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -3,15 +3,27 @@ import java.util.Scanner;
 public class StringExercise {
     public static void main(String[] args) {
 //        1.
+        //STORE VALUE= NOT REASSIGNING THE REFERENCE:
 //        String string = "We don't need no education";
 ////        string = string.replace("education","thought control" );
 ////        string = string.replace("We don't need no education","Check \"this\" out!, \"s inside of \"s!");
 ////        string = string.replace("We don't need no education","In windows, the main drive is usually C:\\\"");
-//        string = string.replace("We don't need no education","I can do backslashes \\, double backslashes \\\\,\n" +
-//                "and the amazing triple backslash \\\\\\!");
+////        string = string.replace("We don't need no education","I can do backslashes \\, double backslashes \\\\,\n" +
+////                "and the amazing triple backslash \\\\\\!");
 //        System.out.println(string);
 
+        String first = "We don't need no education\nWe don't need no thought control"; // \n = new line
+        System.out.println(first);
 
+        String second = "Check \"this\" out!, \"s inside of \"s!"; // use \ in front of " in string to show them
+        System.out.println(second);
+
+        String third = "In windows, the main drive is usually C:\\"; // \ also works for \
+        System.out.println(third);
+
+        String fourth = "I can do backslashes \\, double backslashes \\\\,\n" + // one \ for each character
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(fourth);
     }
 }
 //        2.
