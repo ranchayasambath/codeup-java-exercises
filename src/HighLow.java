57c04895986bb007b47fbae97f5202fb7980b76b
ranchayasambath <ranchayasambath@gmail.com>
2022-02-22 10:45:54 -0600

    completed Method exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,21 +1,18 @@
-
 import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
-        // Create a random number generator
         numberGuess();
     }
-        // Use Scanner for getting input from user
         public static void numberGuess(){
             Scanner scanner = new Scanner(System.in);
             int number = (int) (Math.random()*100);
-            int guess = -1;
+            int guess = 0;
             while (guess != number) {
                 System.out.print("Enter your number 1 to 100: ");
                 guess = scanner.nextInt();
-                if (guess <= 0 || guess > 100 ){
-                    System.out.println("invalid selection!");
+                if (guess < 1 || guess > 100 ){
+                    System.out.println("Invalid selection.\nTry again!");
                 }
                 else if (guess < number) {
                     System.out.println("Too low, please try again");
@@ -24,7 +21,7 @@ public class HighLow {
                     System.out.println("Too high, please try again");
                 }
                 else {
-                    System.out.println("Correct, the number was " + number);
+                    System.out.println("Correct, the number was: " + number + ".");
                 }
             }
         }
