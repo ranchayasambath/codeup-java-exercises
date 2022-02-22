4603e2cabf2ae9999b87530b78c16793e19ea73f
ranchayasambath <ranchayasambath@gmail.com>
2022-02-22 09:25:38 -0600

    completed Method exercise

---
+++ src/HighLow.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class HighLow {
+    public static void main(String[] args) {
+        // Create a random number generator
+        numberGuess();
+    }
+        // Use Scanner for getting input from user
+        public static void numberGuess(){
+            Scanner scanner = new Scanner(System.in);
+            int number = (int) (Math.random()*100);
+            int guess = -1;
+            while (guess != number) {
+                System.out.print("Enter your number 1 to 100: ");
+                guess = scanner.nextInt();
+                if (guess < number) {
+                    System.out.println("Too low, please try again");
+                } else if (guess > number) {
+                    System.out.println("Too high, please try again");
+                } else {
+                    System.out.println("Correct, the number was " + number);
+                }
+            }
+        }
+    }
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,24 +1,28 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MethodsExercises {
 
     public static void main(String[] args) {
 //        1.
-        System.out.println("addition: " + addition(1, 4));
-        System.out.println("subtraction: " + subtraction(10, 2));
-        System.out.println("division: " + division(4, 2));
-        System.out.println("multiplication: " + multiplication(10, 2));
-//        System.out.println("recursion: " + recursion(10,2));
-        System.out.println("modulus: " + modulus(10, 4) + " remainders.");
-        //----------------------------------------
+//        System.out.println("addition: " + addition(1, 4));
+//        System.out.println("subtraction: " + subtraction(10, 2));
+//        System.out.println("division: " + division(4, 2));
+//        System.out.println("multiplication: " + multiplication(100, 20));
+//        System.out.println("modulus: " + modulus(10, 4) + " remainders.");
+//-------------------------------------------------------------------------
 //        2.
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your integer: ");
-        int userInput = sc.nextInt();
-        int userInput2 = sc.nextInt();
-//        System.out.println(getInteger(userInput, userInput2));
-
+//        System.out.print("Enter a number between 1 and 10: ");
+//        int userInput = getInteger(1,10);
+//        System.out.print("You've Entered: " + userInput + ".");
+//--------------------------------------------------------------------------
+//        3.
+//        factorial();
+//--------------------------------------------------------------------------
+//        4.
+        diceRoll(1);
     }
+//--------------------------------------------------------------------------
 
     //    1.
     public static int addition(int num1, int num2) {
@@ -33,25 +37,86 @@ public class MethodsExercises {
         return (num1 / num2);
     }
 
-    public static int multiplication(int num1, int num2) {
-//        return(num1*num2);
-        int sum = 0;
-        for (int i = 0; i < num1; i++) {
-            sum = sum + num2;
-        }
-        return sum;
-    }
-//    public static int recursion(int num1, int num2){
+//    public static int multiplication(int num1, int num2) {
+//        return(num1*num2)};
 
+//BONUS:
+//    public static int multiplication(int num1, int num2) {
+//        int result = 0;
+//        for (int i = 0; i < num2; i++) {
+//            result = result + num1;
+//        }
+//        return result;
 //    }
+    public static int multiplication(int num1, int num2) {
+        if (num2 == 1) {
+            return num1;
+        }
+        return num1 + multiplication(num1, num2 - 1);
+    }
+
 
     public static int modulus(int num1, int num2) {
         return (num1 % num2);
     }
-}
-//---------------------------------------------------
+
+//--------------------------------------------------------------------------
 //    2.
-//public static int getInteger(int min, int max) {
-//    if (max <= min) {
-//        System.out.println("you reached limit");
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if (min <= input & input <= max) {
+            return input;
+        } else {
+            System.out.print("Re-enter a number between 1 and 10: ");
+            return getInteger(min, max);
+        }
+    }
+//--------------------------------------------------------------------------
+//    3.
+//    public static void factorial() {
+//        Scanner sc = new Scanner(System.in);
+//        String findFactorial = "y";
+//        while (true) {
+//            if (findFactorial.equalsIgnoreCase("n")) {
+//                System.out.println("Exiting..");
+//                return;
+//            }
+//            System.out.print("Please enter a number between 1 and 10: ");
+//            int input = getInteger(1,10);
+//            long storedFactorial = recursive(input);
+//            System.out.printf("%s!\t=\t%s%n", input, storedFactorial);
+//            System.out.print("Continue? (y/n): ");
+//            findFactorial = sc.next();
+//        }
 //    }
+//    public static int recursive(int num) {
+//        if (num == 1) {
+//            return 1;
+//        }
+//        return num * recursive(num - 1);
+//    }
+//--------------------------------------------------------------------------
+    //        4.
+    public static void diceRoll(int num) {
+        System.out.print("How many sides are your dice?: ");
+        Scanner sc = new Scanner(System.in);
+        int diceSide = sc.nextInt();
+        String reRoll;
+        do {
+            System.out.print("Press r to roll: ");
+            String respond = sc.next();
+            if (respond.equalsIgnoreCase("r")) {
+                int diceOne = (int) (Math.random() * diceSide + 1);
+                int diceTwo = (int) (Math.random() * diceSide + 1);
+                System.out.printf("Rolled:\t%s and %s.%n", diceOne, diceTwo);
+            }
+            System.out.print("Play again? (y/n): ");
+            reRoll = sc.next();
+        } while (reRoll.equalsIgnoreCase("y"));
+        System.out.println("See ya!");
+    }
+}
+//
+
+
