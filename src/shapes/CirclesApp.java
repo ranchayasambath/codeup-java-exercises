1ac3137c37a34c5d3ca3e72b29aab0d42e9d144f
ranchayasambath <ranchayasambath@gmail.com>
2022-02-23 19:56:47 -0600

    refactored exercise 2 with bonus

--- src/shapes/CirclesApp.java
+++ src/shapes/CirclesApp.java
@@ -7,7 +7,6 @@ public class CirclesApp {
     public static int setCounter(){
          return counter++;
     }
-
     public static void main(String[] args) {
         boolean confirm;
         do{
@@ -16,8 +15,8 @@ public class CirclesApp {
             double area = newCircle.getArea();
             double circumference = newCircle.getCircumference();
             setCounter();
-            System.out.printf("Area is: %.3f.%n", area);
-            System.out.printf("Circumference is: %.3f.%n", circumference);
+            System.out.printf("Area is: %.2f.%n", area);
+            System.out.printf("Circumference is: %.2f.%n", circumference);
             confirm = input.yesNo("Continue?");
         }while (confirm);
         System.out.printf("Created %s circle(s).",setCounter());
