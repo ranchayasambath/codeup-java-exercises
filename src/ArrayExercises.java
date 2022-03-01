7932375aff8c67d379711812f735bce243dd04b8
ranchayasambath <ranchayasambath@gmail.com>
2022-03-01 09:35:39 -0600

    in class warmup

--- src/ArrayExercises.java
+++ src/ArrayExercises.java
@@ -2,6 +2,35 @@ import java.util.Arrays;
 
 public class ArrayExercises {
     public static void main(String[] args) {
+        //--------------------------------------------------------------------------------------------------------------------
+        // In-class warm-up exercise:
+
+//        Write a program that creates an array, and then prints the sum of the even and odd integers in a new array [sumOdd, sumEven]
+//        example: [2,4,8,5,1,2];
+//        output; [6,16]
+//        reason: 2+4+8+2 = 16
+//        5+1 = 6
+
+        int[] warmup = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] oddEven = new int[2];
+        int odd = 0;
+        int even = 0;
+        for (int num : warmup) {
+            if (num % 2 == 0) {
+                even += num;
+            } else {
+                odd += num;
+            }
+        }
+        oddEven[0]= odd;
+        oddEven[1]= even;
+        System.out.println(Arrays.toString(oddEven));
+
+//--------------------- Calling method --------------------------------------------------------------------------------
+        System.out.println(Arrays.toString(warmupMethod(warmup)));
+//---------------------------------------------------------------------------------------------------------------------
+
+
 //        1.
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(Arrays.toString(numbers));
@@ -16,9 +45,7 @@ public class ArrayExercises {
         Person john = new Person("John");
         addPerson(person, john);
 
-        // in-class exercise:
-//        System.out.println(sumAll());
-    }
+ }
 
     //accept Person Array object and Person object.
     public static void addPerson(Person[] person, Person nextPerson) {
@@ -30,9 +57,25 @@ public class ArrayExercises {
         for (Person addedPerson : newArray) {
             System.out.println(addedPerson.getName());
         }
-
-
-
+    }
+//------------------------------IN-class warm-up Method Calling--------------------------------------------------------
+    public static int[] warmupMethod(int[] ogArr) {
+        int[] oddEven = new int[2];
+        int odd = 0;
+        int even = 0;
+        for (int num : ogArr) {
+            if (num % 2 == 0) {
+                even += num;
+            } else {
+                odd += num;
+            }
+        }
+        oddEven[0] = odd;
+        oddEven[1] = even;
+//        System.out.println(Arrays.toString(oddEven));
+        return oddEven;
+    }
+//---------------------------------------------------------------------------------------------------------------------
     // in-class exercise:
 //    public static int sumAll(){
 //        int sum=0;
@@ -42,5 +85,5 @@ public class ArrayExercises {
 //        }
 //        return sum;
 //    }
-}
+
 }
