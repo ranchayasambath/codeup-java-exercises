560b459b4276e4ad84dfca0c3263bff6c6dc312b
ranchayasambath <ranchayasambath@gmail.com>
2022-03-07 15:59:46 -0600

    better_fix completed

---
+++ src/annotations/Person.java
@@ -0,0 +1,20 @@
+package annotations;
+
+/**
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+public abstract class Person {
+    protected String firstName;
+    protected String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String fullName() {
+        return String.format("%s %s", firstName, lastName);
+    }
+
+    public abstract String sortName();
+}
