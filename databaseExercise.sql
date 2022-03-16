5dcdc9a5c1f2784190fa636c2186e1bda8e2ecbe
ranchayasambath <ranchayasambath@gmail.com>
2022-03-16 09:36:26 -0500

    complted databaseExercise

---
+++ databaseExercise.sql
@@ -0,0 +1,23 @@
+1.Create a new database named testing123
+CREATE DATABASE testing123;
+2.List all databases, make sure your new database is in the list.
+SHOW DATABASES;
+3.Show the SQL to recreate your database.
+SELECT database();
+4.Switch to your new database using the USE statement.
+USE testing123;
+5.Delete your new database.
+DROP DATABASE testing123;
+6.List all databases, make sure your new database is not in the list.
+SHOW DATABASES;
+7.Try dropping your database again, make sure the command succeeds even though the database is now missing.
+DROP DATABASE IF EXISTS testing123;
+8.Create a new database called codeup_test_db and user codeup_test_user.
+
+CREATE DATABASE codeup_test_db;
+CREATE USER 'codeup_test_user'@'localhost' IDENTIFIED BY 'password';
+
+Give codeup_test_user all permissions only on codeup_test_db,Make sure to remember this new user''s password.
+
+USE codeup_test_db;
+GRANT ALL ON codeup_test_db TO 'codeup_test_user'@'localhost';
