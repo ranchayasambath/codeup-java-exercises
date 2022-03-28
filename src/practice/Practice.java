a5ae504748d7993c22fe29d5077a81843355bf0d
ranchayasambath <ranchayasambath@gmail.com>
2022-03-28 16:28:20 -0500

    practice exercise

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -4,10 +4,18 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println(notString("cake"));
         System.out.println(notString("not bad"));
+        System.out.println(diff21(10));
     }
     public static String notString(String str) {
         if (str.length() >= 3 && str.substring(0, 3).equals("not"))
             return str;
         return "not "+str;
     }
+//Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+    public static int diff21(int n) {
+        if(n>21)
+            return 2*(n-21);
+        return 21-n;
+    }
+
 }
